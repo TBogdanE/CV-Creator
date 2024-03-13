693a04b625d1210361dd263db94a78c48bfc8d27
Toma Bogdan <bogdantoma2000@gmail.com>
2024-03-13 20:43:17 +0200

    add cv class for data management

---
+++ CV-creator/src/functions/data.js
@@ -0,0 +1,18 @@
+class CVdata {
+  constructor() {
+    this.name = "Your name";
+    this.email = "Your email";
+    this.phone = "+4075 xxx xxxx";
+    this.education = "School, University or Work";
+    this.date = "1.1.1990";
+    this.experience = "work";
+    this.workposition = "position";
+    this.resp = "responsibilities";
+    this.workstartdate = "1.1.1990";
+    this.workenddate = "1.1.1990";
+  }
+}
+
+const personCV = new CVdata();
+
+export { personCV };
